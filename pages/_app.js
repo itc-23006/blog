aa24336e4f4c8fd825e47c4c9a17d836beb3ed65
itc-23006 <n23006@std.it-college.ac.jp>
2023-12-14 07:03:12 +0000

    コンポネートの追加。

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '@/styles/globals.css'
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import Layout from '../components/layout.js'
+export default function App ({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
