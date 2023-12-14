b4716a616bcddc570c1f10db87d28727f565cdf8
itc-23006 <n23006@std.it-college.ac.jp>
2023-12-14 08:05:14 +0000

    image0nの初期値をfalseと指定する。

--- components/hero.js
+++ components/hero.js
@@ -1,8 +1,9 @@
-const Hero = ({ title, subtitle }) => {
+const Hero = ({ title, subtitle, image0n = false }) => {
   return (
     <div>
       <h1>title</h1>
       <p>subtitle</p>
+      {image0n && <figure> [画像] </figure>}
     </div>
   )
 }
