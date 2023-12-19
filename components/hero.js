a92783cbcbc027062699413b26ded983489953ab
itc-23006 <n23006@std.it-college.ac.jp>
2023-12-19 06:40:37 +0000

    classの追加。

--- components/hero.js
+++ components/hero.js
@@ -1,10 +1,15 @@
-const Hero = ({ title, subtitle, image0n = false }) => {
+import styles from 'styles/hero.module.css'
+
+const Hero = ({ title, subtitle, imageOn = false }) => {
   return (
-    <div>
-      <h1>title</h1>
-      <p>subtitle</p>
-      {image0n && <figure> [画像] </figure>}
+    <div className={styles.text}>
+      <h1 className={styles.title}>{title}</h1>
+      <p className={styles.subtitle}>{subtitle}</p>
+      <h1>{title}</h1>
+      <p>{subtitle}</p>
+      {imageOn && <figure> [画像] </figure>}
     </div>
   )
 }
+
 export default Hero
