32225552af99a6ab2f5667664ce9dbecab700867
itc-23006 <n23006@std.it-college.ac.jp>
2023-12-20 06:54:54 +0000

    P126
    <h3>の追加。

--- components/container.js
+++ components/container.js
@@ -1,5 +1,10 @@
 import styles from 'styles/container.module.css'
 
 export default function Container ({ children, large = false }) {
-  return <div className={large ? styles.large : styles.default}>{children}</div>
+  return (
+    <div className={large ? styles.large : styles.default}>
+      <h3 className={styles.heading}>Contact</h3>
+      <address>cube@web.mailaddress</address>
+    </div>
+  )
 }
