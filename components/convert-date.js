742475bf3a66c7cff158422aa809bc572d8dc225
itc-23006 <n23006@std.it-college.ac.jp>
2024-01-11 05:25:20 +0000

    コードの修正。

--- components/convert-date.js
+++ components/convert-date.js
@@ -1,12 +1,12 @@
 import { parseISO, format } from 'date-fns'
 import ja from 'date-fns/locale/ja'
 
-export default function ConvertDate ({ dateISO }) {
+const ConverDate = ({ dateISO }) => {
   return (
     <time dateTime={dateISO}>
-      {format(parseISO(dateISO), 'yyyy年MM月dd日', {
-        locale: ja
-      })}
+      {format(parseISO(dateISO), 'yyyy年MM月dd日', { locale: ja })}
     </time>
   )
 }
+
+export default ConverDate
